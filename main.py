da04ad5e10dec9e3f2dfbed30d74a204d3fa8d30
kazmi1012 <kazmigurung1012@gmail.com>
2026-05-14 15:32:48 +0545

    ASCII art

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import random
+from stages import render_stage
+import json
+
 class Hangman():
 
     def __init__(self, word, hint):
@@ -5,13 +9,48 @@ class Hangman():
         self.hint = hint
         self.user_answer = ['_'] * len(self.word)
         self.dashes = '-' * 30
+        self.misses = 0
+        self.MAX_MISSES = 6
+        self.entered_letters = set()
+
 
     def print_header(self):
         print(self.dashes)
-        print('\nH A N G M A N')
+        print('\tH A N G M A N')
         print(self.dashes)
 
+    
+    def display_status(self):
+
+        print('\nCurrent:', " ".join(self.user_answer))
+        print(f'Hint: {self.hint}')
+        print("Already Guessed:", ", ".join(sorted(self.entered_letters)))
+        print(f'Misses: {self.misses} / {self.MAX_MISSES}')
+        
+    
+
+
+    # def check(self, guess):
+    #     if not guess.isalpha() or len(guess) != 1:
+    #         print( "Invalid input! You can only enter alphabets (A-Z).")
+    #         return False 
+    #     return True
+
+
     def process_guess(self, guess):
+
+        # self.check()
+
+        if guess in self.entered_letters:
+            print("You've already guessed that letter!")
+        else:
+            self.entered_letters.add(guess)
+
+
+        if not guess.isalpha() or len(guess) != 1:
+            print( "Invalid input! You can only enter alphabets (A-Z).")
+            return
+
 
         if guess in self.word:
 
@@ -24,37 +63,73 @@ class Hangman():
 
         else:
             print("\nIncorrect!")
+            self.misses += 1
+
+
 
     def play(self):
 
+        
         self.print_header()
 
         while True:
+            render_stage(self.misses)
+            self.display_status()
 
-            print('\nCurrent:', " ".join(self.user_answer))
             guess = input("\nGuess a letter: ").strip().upper()
             self.process_guess(guess)
             print(f'\n{self.dashes}')
-            if self.is_won():
+           
+            if self.is_won() or self.is_lost():
+                render_stage(self.misses)
                 self.display_result()
                 break
 
+            
+
+
     def is_won(self):
         return '_' not in self.user_answer
+    
+    def is_lost(self):
+        return self.misses >= self. MAX_MISSES
+
     
     def display_result(self):
         if self.is_won():
             print(f'Word:', " ".join(self.user_answer) )
             print('You won!')
-        self.dashes()
+            print(self.dashes)
+
+        if self.is_lost():
+            print(f'Misses: {self.misses} / {self.MAX_MISSES}')
+            print('You lose!')
+            print(f'The word was: {self.word}')
+    
+    def print_goodbye(self):
+        print(self.dashes)
+        print("\tG O O D B Y E")
+        print(self.dashes)
 
 
 
-        
+with open("words.json", "r") as f:
+    words_list = json.load(f)
 
+while True:
 
-word = "PROGRAMMING"
-hint = "The act of providing coded instructions to computers"
+    selected = random.choice(words_list)
 
-game = Hangman(word, hint)
-game.play()
+    word = selected["word"].upper()
+
+    hint = selected["hint"]
+
+    game = Hangman(word, hint)
+
+    game.play()
+
+    again = input("\nDo you want to play again? (Y/N): ").strip().upper()
+
+    if again != "Y":
+        game.print_goodbye()
+        break
